eaf1f7ac4313a1e2b6d548fb714938ea881b33e5
Ami Rahav <arahav@boundlessgeo.com>
2019-02-05 17:19:05 +0000

    Compat with newer versions of es dsl

--- elasticsearch_app/search.py
+++ elasticsearch_app/search.py
@@ -278,8 +278,8 @@ class LayerIndex(DocType):
     geogig_link = Keyword()
     has_time = Boolean()
 
-    class Meta:
-        index = 'layer-index'
+    class Index:
+        name = 'layer-index'
 
 
 def create_layer_index(layer):
@@ -395,8 +395,8 @@ class MapIndex(DocType):
     num_ratings = Integer()
     num_comments = Integer()
 
-    class Meta:
-        index = 'map-index'
+    class Index:
+        name = 'map-index'
 
 
 def create_map_index(map):
@@ -502,8 +502,8 @@ class DocumentIndex(DocType):
     num_ratings = Integer()
     num_comments = Integer()
 
-    class Meta:
-        index = 'document-index'
+    class Index:
+        name = 'document-index'
 
 
 def create_document_index(document):
@@ -564,8 +564,8 @@ class ProfileIndex(DocType):
     documents_count = Integer()
     profile_detail_url = Text()
 
-    class Meta:
-        index = 'profile-index'
+    class Index:
+        name = 'profile-index'
 
 
 def create_profile_index(profile):
@@ -629,8 +629,8 @@ class GroupIndex(DocType):
     )
     detail_url = Text()
 
-    class Meta:
-        index = 'group-index'
+    class Index:
+        name = 'group-index'
 
 
 def create_group_index(group):

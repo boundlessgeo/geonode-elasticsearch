b21613d71c4c57eab27153bf534dc22300258b74
Travis Brundage <travislbrundage@gmail.com>
2019-04-30 09:09:30 -0700

    Adding autocomplete endpoint to filter results from elastic search and serialize data for the format select2 expects

--- elasticsearch_app/urls.py
+++ elasticsearch_app/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import patterns, url
-from elasticsearch_app.views import empty_page, elastic_search
+from elasticsearch_app import views
 
 urlpatterns = patterns(
     '',
     url(r'^api/(?P<resourcetype>[^/]+)/search/$',
-        elastic_search,
+        views.elastic_search,
         name='elastic_search'),
+    url(r'^autocomplete/groups',
+        views.suggest_search_groups,
+        name='autocomplete_groups'),
+    url(r'^autocomplete/people',
+        views.suggest_search_people,
+        name='autocomplete_people'),
     url(r'^autocomplete',
-        empty_page,
-        name='autocomplete_override')
+        views.suggest_search,
+        name='autocomplete')
 )
